c25fce89ff3a58a4839c7c5f85b1e126f60f1a39
chrisspenceratx <chrisspenceratx@gmail.com>
2023-01-24 13:37:11 -0600

    small comment change

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -14,6 +14,7 @@ export const MovieCard = ({ movie, onMovieClick }) => {
   );
 };
 
+// comment written to test new branch //
 // Here is where we define all the props constraints for the MovieCard
 MovieCard.propTypes = {
   movie: PropTypes.shape({
