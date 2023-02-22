b75d776336e71078731093f1edbcab41802d8766
chrisspenceratx <chrisspenceratx@gmail.com>
2023-02-22 12:52:33 -0600

    LoginView doc created, login form code put in

---
+++ src/components/login-view.jsx
@@ -0,0 +1,21 @@
+export const LoginView = () => {
+    return (
+      <form>
+        <label>
+          Username:
+          <input type="text" />
+        </label>
+        <label>
+          Password:
+          <input type="password" />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    );
+  };
+
+  const [user, setUser] = useState(null);
+
+  if (!user) {
+    return <LoginView />;
+  }
